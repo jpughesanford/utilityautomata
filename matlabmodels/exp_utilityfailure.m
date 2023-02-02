ba625bcfcaa6ac219e1f96deac1884cfc909b366
hannah-griggs <griggshannah113@gmail.com>
2023-02-02 17:13:00 -0500

    Updates to pop models

--- matlabmodels/exp_utilityfailure.m
+++ matlabmodels/exp_utilityfailure.m
@@ -3,12 +3,14 @@ close all
 N = 32+1;
 
 % four probabilities make up the dynamics
-p_join      = 1.00; % probability that a de-central node becomes central if its neighbor is central. Compounds linearly with how many neighbors are central
+%p_join      = randfield(N,N,1,5)/10; % probability that a de-central node becomes central if its neighbor is central. Compounds linearly with how many neighbors are central
+%p_join(1:10,1:10)=0;
+
 p_outtage   = 0.005; % probability that a centralized nodes undergoes an outtage
 p_recover   = 1.00; % probability that an outtage is fixed. this leaves the tile decentral
 
 %%%%%%%%%%%% create initial state %%%%%%%%%%%%
-
+%%
 % make state of zeros
 s = zeros(N,N);
 s(round(N/2),round(N/2)) = 1;
@@ -18,8 +20,15 @@ e = zeros(N^2,4);
 
 %%%%%%%%%%%% Population and landscape infrastructure %%%%%%%%%%%%
 
-population = randfield(N,N,1,5);
-landscape = randfield(N,N,50,5);
+population = zeros(N,N);
+population(round(N/2),round(N/2)) = .5;
+population(round(N/3),round(N/3)) = 0.3;
+D = 0.1;
+dt = 0.001;
+
+lands = randfield(N,N,1,5);
+[landgradsx, landgradsy] = gradient(lands)
+steepness = sqrt(landgradsy.^2+landgradsx.^2);
 
 % Wishlist: find a way to get the gradient of the landscape one
 %           so I can use that as my metric for increasing or 
@@ -34,15 +43,20 @@ landscape = randfield(N,N,50,5);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%
 figure;
 resizefigure(2,1);
-p = zeros(3,1);
+
 for k = 1:1000*N^2
     
+    population = rk4(population,D,dt);
+
+    p_join = population;
+
     [s,e] = update(s,e,p_join,p_outtage,p_recover);
     
     if mod(k,N^2)==1
-        plotstate(s,e);
+        plotstate(s,e,p_join);
     end
     
 end
@@ -55,9 +69,8 @@ function [s,e,outtagesize] = update(s,e,p1,p2,p3)
 
 [Ny,Nx] = size(s);
 Nn = size(e,2);
-% neighborsum = imfilter(s,[0 1 0; 1 0 1; 0 1 0],'circular');
-% neighborsum = conv2(s,[0 1 0; 1 0 1; 0 1 0],'same');
-neighborsum = round(conv_fft2(s,[0 1 0; 1 0 1; 0 1 0],'wrap'));
+neighborsum = conv2(s,[0 1 0; 1 0 1; 0 1 0],'same'); % USE FOR SOLID BOUNDARY
+% neighborsum = round(conv_fft2(s,[0 1 0; 1 0 1; 0 1 0],'wrap')); % USE FOR PERIODIC BOUNDARIES
 outtagesize = 0;
 
 k = randi(Ny*Nx);
@@ -76,7 +89,7 @@ if sij == -1
 elseif sij == 0
     
     % become central with probability n*p2, where n is the number of neighboring central nodes minus the number of neighboring out nodes
-    if rand < neighborsum(i,j)*p1
+    if rand < neighborsum(i,j)*p1(i,j)
         
         % set pixel to be central
         s(i,j) = 1;
@@ -160,7 +173,7 @@ end
 
 % Field maker
 function f = randfield(Nx,Ny,Nt,cutoff)
-    
+   
     % return a smooth, randomly varying field between zero and 1. As cutoff
     % increases, the structure of the field becomes smaller wavelength. 
     f = fftshift(fftn(rand(Ny,Nx,Nt)));
@@ -172,7 +185,33 @@ function f = randfield(Nx,Ny,Nt,cutoff)
     
 end
 
-function plotstate(s,e)
+function P = rk4(P,D,h)
+ 
+% P : population
+% D: diffusion coefficient
+% h : timestep
+ 
+halfh = h / 2;
+h6 = h/6;
+ 
+p1 = dPdt(P,D);
+p2 = dPdt(P + halfh * p1,D); 
+p3 = dPdt(P + halfh * p2,D); 
+p4 = dPdt(P + h * p3,D); 
+P = P + (p1 + 2*p2 + 2*p3 + p4) * h6;
+ 
+end
+function dP = dPdt(P,D)
+ 
+lapP = del2(P);
+ 
+dP = P.*(1-P)+D*lapP;
+dP([1 end],:)=0;
+dP(:,[1 end])=0;
+
+end
+
+function plotstate(s,e,p_join)
 
 % imagesc(s)
 % hold on
@@ -186,8 +225,11 @@ function plotstate(s,e)
 [Ny,Nx] = size(s);
 
 subplot(1,2,1)
-imagesc(s);
+imagesc(p_join);
+set(gca,'YDir','normal')
 hold on
+[yyy,xxx] = ind2sub(size(s),find(s==-1));
+scatter(xxx,yyy,150,'rs','filled')
 reccursiveplot(s,e,round(numel(s)/2));
 hold off
 axis equal
@@ -203,7 +245,7 @@ p(3) = sum(s(:)==-1);
 P = p/sum(p);
 bar(P);
 xticks(1:3);
-xticklabels({'Decentralized','Centralized','Out'});
+xticklabels({'decentral','central','out'});
 text((1:numel(P))-.2,P+.02,num2cell(P))
 title('Average (in time) percent of grid that is...')
 text((1:numel(P))-.2,P+.02,num2cell(P))
@@ -211,7 +253,6 @@ ylim([0 1]);
 drawnow
 
 end
-
 function reccursiveplot(s,e,k)
 
 [y1,x1] = ind2sub(size(s),k);
@@ -227,6 +268,114 @@ end
 end
 
 function newmap = rwb(m)
+%BLUEWHITERED   Blue, white, and red color map.
+%   BLUEWHITERED(M) returns an M-by-3 matrix containing a blue to white
+%   to red colormap, with white corresponding to the CAXIS value closest
+%   to zero.  This colormap is most useful for images and surface plots
+%   with positive and negative values.  BLUEWHITERED, by itself, is the
+%   same length as the current colormap.
+%
+%   Examples:
+%   ------------------------------
+%   figure
+%   imagesc(peaks(250));
+%   colormap(bluewhitered(256)), colorbar
+%
+%   figure
+%   imagesc(peaks(250), [0 8])
+%   colormap(bluewhitered), colorbar
+%
+%   figure
+%   imagesc(peaks(250), [-6 0])
+%   colormap(bluewhitered), colorbar
+%
+%   figure
+%   surf(peaks)
+%   colormap(bluewhitered)
+%   axis tight
+%
+%   See also HSV, HOT, COOL, BONE, COPPER, PINK, FLAG,
+%   COLORMAP, RGBPLOT.
+if nargin < 1
+    m = size(get(gcf,'colormap'),1);
+end
+
+bottom = [0 0 0.5];
+botmiddle = [0 0.5 1];
+middle = [1 1 1];
+topmiddle = [1 0 0];
+top = [0.5 0 0];
+
+% bottom = [0 0 0.25];
+% botmiddle = [0 0 0.5];
+% middle = [1 1 1];
+% topmiddle = [0.5 0 0];
+% top = [0.25 0 0];
+
+% Find middle
+lims = get(gca, 'CLim');
+% Find ratio of negative to positive
+if (lims(1) < 0) & (lims(2) > 0)
+    % It has both negative and positive
+    % Find ratio of negative to positive
+    ratio = abs(lims(1)) / (abs(lims(1)) + lims(2));
+    neglen = round(m*ratio);
+    poslen = m - neglen;
+    
+    % Just negative
+    new = [bottom; botmiddle; middle];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, neglen);
+    newmap1 = zeros(neglen, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap1(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+    % Just positive
+    new = [middle; topmiddle; top];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, poslen);
+    newmap = zeros(poslen, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+    % And put 'em together
+    newmap = [newmap1; newmap];
+    
+elseif lims(1) >= 0
+    % Just positive
+    new = [middle; topmiddle; top];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, m);
+    newmap = zeros(m, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+else
+    % Just negative
+    new = [bottom; botmiddle; middle];
+    len = length(new);
+    oldsteps = linspace(0, 1, len);
+    newsteps = linspace(0, 1, m);
+    newmap = zeros(m, 3);
+    
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        newmap(:,i) = min(max(interp1(oldsteps, new(:,i), newsteps)', 0), 1);
+    end
+    
+end
 %BLUEWHITERED   Blue, white, and red color map.
 %   BLUEWHITERED(M) returns an M-by-3 matrix containing a blue to white
 %   to red colormap, with white corresponding to the CAXIS value closest
